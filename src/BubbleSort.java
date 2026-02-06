f3e37836d984a068de1a876ed8952bb21a85fe22
annabui9 <annabui1009@gmail.com>
2026-02-06 00:20:23 -0600

    [commented out code i used to test bubblesort] Created interface SortingAlgorithm, implemented SortingAlgorithm in each class, created and tested BubbleSort (i commented out the code where i tested to make sure the algorithm worked -- i will be doing this for all classes)

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -17,30 +17,30 @@ public class BubbleSort implements SortingAlgorithm {
         return input;
     }
 
-    //PRINTING ARRAY (to test)
-    public static String printArray(int[] input){
-        String array = "[";
-
-        for (int i = 0; i < input.length; i++) {
-
-            array = array + input[i];
-            if (i < input.length - 1) {
-                array = array +  " , ";
-            }
-
-        }
-        array = array + "]";
-        return array;
-    }
-
-    //TESTING ALGORITHM
-    public static void main(String args[]){
-        BubbleSort bs = new BubbleSort();
-        int[] input = {5, 10, 60, 0, -1, 34, 6, 10};
-        // Output should be: [-1, 0, 5, 6, 10, 10, 34, 60]
-
-        System.out.println(printArray(bs.sorty(input)));
-
-    }
+//    //PRINTING ARRAY (to test)
+//    public static String printArray(int[] input){
+//        String array = "[";
+//
+//        for (int i = 0; i < input.length; i++) {
+//
+//            array = array + input[i];
+//            if (i < input.length - 1) {
+//                array = array +  " , ";
+//            }
+//
+//        }
+//        array = array + "]";
+//        return array;
+//    }
+//
+//    //TESTING ALGORITHM
+//    public static void main(String args[]){
+//        BubbleSort bs = new BubbleSort();
+//        int[] input = {5, 10, 60, 0, -1, 34, 6, 10};
+//        // Output should be: [-1, 0, 5, 6, 10, 10, 34, 60]
+//
+//        System.out.println(printArray(bs.sorty(input)));
+//
+//    }
 
 }
